164c581a1b4a0343b894544f9723fff0106e9ea4
sominlee92 <sominlee92@gmail.com>
2021-03-16 21:17:10 +0900

    calendar test
    calendar test

--- lee.somin/js/app.js
+++ lee.somin/js/app.js
@@ -1,12 +1,52 @@
-//Document Ready
 
-
+// Document Ready
 $(()=>{
 
-	$(document)
+   checkUserId();
 
-	.on("submit","#signin-form",function(e){
-		e.preventDefault();
-		checkSigninForm();
-	})
-});
+   $(document)
+
+
+   /* FORM SUBMITS */
+   .on("submit","#signin-form",function(e){
+      e.preventDefault();
+      checkSigninForm();
+   })
+
+
+   /* ANCHOR CLICKS */
+   .on("click",".js-logout",function(e){
+      sessionStorage.removeItem('userId');
+      checkUserId();
+   })
+
+
+   /* DATA ACTIVATE */
+   .on("click","[data-activate]",function(e){
+      let target = $(this).data("activate");
+      $(target).addClass("active");
+   })
+   .on("click","[data-deactivate]",function(e){
+      let target = $(this).data("deactivate");
+      $(target).removeClass("active");
+   })
+   .on("click","[data-toggle]",function(e){
+      let target = $(this).data("toggle");
+      $(target).toggleClass("active");
+   })
+
+
+
+
+
+
+
+
+
+   $("[data-template]").each(function(){
+      let id = $(this).data("template");
+      let template = $(id).html();
+      $(this).html(template);
+   })
+
+}); 
